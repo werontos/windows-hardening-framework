20c40472ae9dfec2b18c405f11acfac42df94495
werontos <koirainingforever@gmail.com>
2026-05-22 09:22:58 +0000

    Update Harden-Endpoint-DC.ps1

--- scripts/Harden-Endpoint-DC.ps1
+++ scripts/Harden-Endpoint-DC.ps1
@@ -74,8 +74,8 @@ Set-DwordValue "HKLM:\System\CurrentControlSet\Control\Lsa" "SCENoApplyLegacyAud
 
 # Device Guard / Credential Guard
 
-Set-DwordValue "HKLM:\SOFTWARE\Policies\Microsoft\Windows\DeviceGuard" "LsaCfgFlags" 0
-Set-DwordValue "HKLM:\SYSTEM\CurrentControlSet\Control\Lsa" "LsaCfgFlags" 0
+Set-DwordValue "HKLM:\SOFTWARE\Policies\Microsoft\Windows\DeviceGuard" "LsaCfgFlags" 1
+Set-DwordValue "HKLM:\SYSTEM\CurrentControlSet\Control\Lsa" "LsaCfgFlags" 1
 #Set-DwordValue "HKLM:\SOFTWARE\Policies\Microsoft\Windows\DeviceGuard" "DeployConfigCIPolicy" 1
 # WARNING: Requires signed WDAC policy — dangerous without proper CI policy configured
 
